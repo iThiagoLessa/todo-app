de2e11fc31eb9bc963aaee1ddda4185833ad082a
thiago <ithiagolessa@gmail.com>
2021-04-15 22:56:15 -0300

    adicionando e removendo do banco com axios

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import IconButton from "../template/iconButton";
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+export default (props) => {
+  const renderRows = () => {
+    const list = props.list || [];
+    return list.map((todo) => (
+      //esse _id é gerado pelo mongo, é uma chave dele
+      <tr key={todo._id}>
+        <td>{todo.description}</td>
+        <td>
+          <IconButton
+            style="danger"
+            icon="trash-o"
+            onClick={() => props.handleRemove(todo)}
+          />
+        </td>
+      </tr>
+    ));
+  };
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Decrição</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>{renderRows()}</tbody>
+    </table>
+  );
+};
