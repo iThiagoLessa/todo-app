75e67ba1ecc11183b45d3d03f9ac630e3b555385
thiago <ithiagolessa@gmail.com>
2021-04-16 22:34:26 -0300

    Conclusão primeira parte curso

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -16,13 +16,19 @@ export default class Todo extends Component {
     this.handleRemove = this.handleRemove.bind(this);
     this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
     this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleClear = this.handleClear.bind(this);
 
     this.refresh();
   }
-  refresh() {
-    axios.get(`${URL}?sort=-createdAt`).then((resp) => {
-      this.setState({ ...this.state, description: "", list: resp.data });
+  refresh(description = "") {
+    const search = description ? `&description__regex=/${description}/` : "";
+    axios.get(`${URL}?sort=-createdAt${search}`).then((resp) => {
+      this.setState({ ...this.state, description, list: resp.data });
     });
+  }
+  handleSearch() {
+    this.refresh(this.state.description);
   }
   //copiando o estado com o spread e atualizando o description
   handleChange(e) {
@@ -34,17 +40,22 @@ export default class Todo extends Component {
     axios.post(URL, { description }).then((resp) => this.refresh());
   }
   handleRemove(todo) {
-    axios.delete(`${URL}/${todo._id}`).then((resp) => this.refresh());
+    axios
+      .delete(`${URL}/${todo._id}`)
+      .then((resp) => this.refresh(this.state.description));
   }
   handleMarkAsDone(todo) {
     axios
       .put(`${URL}/${todo._id}`, { ...todo, done: true })
-      .then((resp) => this.refresh());
+      .then((resp) => this.refresh(this.state.description));
   }
   handleMarkAsPending(todo) {
     axios
       .put(`${URL}/${todo._id}`, { ...todo, done: false })
-      .then((resp) => this.refresh());
+      .then((resp) => this.refresh(this.state.description));
+  }
+  handleClear() {
+    this.refresh();
   }
   render() {
     return (
@@ -54,6 +65,8 @@ export default class Todo extends Component {
           description={this.state.description}
           handleAdd={this.handleAdd}
           handleChange={this.handleChange}
+          handleSearch={this.handleSearch}
+          handleClear={this.handleClear}
         />
         <TodoList
           list={this.state.list}
